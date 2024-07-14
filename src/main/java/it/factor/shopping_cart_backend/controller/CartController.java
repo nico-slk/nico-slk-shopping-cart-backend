719a3a5b74e045dba0a5facf6c2ea2c033342162
nico-slk <nico1991.6@gmail.com>
2024-07-13 23:21:04 -0300

    create cart and delete product of a cart

--- src/main/java/it/factor/shopping_cart_backend/controller/CartController.java
+++ src/main/java/it/factor/shopping_cart_backend/controller/CartController.java
@@ -1,9 +1,29 @@
 package it.factor.shopping_cart_backend.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import it.factor.shopping_cart_backend.model.Cart;
+import it.factor.shopping_cart_backend.repository.CartRepository;
+import it.factor.shopping_cart_backend.service.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/cart")
 public class CartController {
+
+    @Autowired
+    private CartService cartService;
+
+    @PostMapping("/")
+    public ResponseEntity<Cart> addCart(@RequestBody Cart cart) {
+        return ResponseEntity.ok(cartService.addCart(cart));
+    }
+
+    @DeleteMapping("/{cartId}/producto/{productId}")
+    public ResponseEntity<String> deleteCart(@PathVariable("cartId") UUID cartId, @PathVariable("productId") UUID productId) {
+        cartService.deleteProduct(cartId, productId);
+        return ResponseEntity.ok("Producto eliminado con éxito del carrito.");
+    }
 }
