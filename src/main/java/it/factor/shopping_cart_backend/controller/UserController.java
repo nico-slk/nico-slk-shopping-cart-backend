15196be7473ec2de88d54793b3b6b8d58fe69dc3
nico-slk <nico1991.6@gmail.com>
2024-07-13 23:40:22 -0300

    get user by id

--- src/main/java/it/factor/shopping_cart_backend/controller/UserController.java
+++ src/main/java/it/factor/shopping_cart_backend/controller/UserController.java
@@ -1,9 +1,24 @@
 package it.factor.shopping_cart_backend.controller;
 
+import it.factor.shopping_cart_backend.model.User;
+import it.factor.shopping_cart_backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<User> getUser(@PathVariable UUID userId) {
+        return ResponseEntity.ok(userService.getUser(userId));
+    }
 }
--- src/main/java/it/factor/shopping_cart_backend/service/UserService.java
+++ src/main/java/it/factor/shopping_cart_backend/service/UserService.java
@@ -1,7 +1,19 @@
 package it.factor.shopping_cart_backend.service;
 
+import it.factor.shopping_cart_backend.model.User;
+import it.factor.shopping_cart_backend.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User getUser(UUID userId) {
+        return userRepository.findById(userId).orElseThrow(() -> new EntityNotFoundException("Usuario no encontrado"));
+    }
 }
