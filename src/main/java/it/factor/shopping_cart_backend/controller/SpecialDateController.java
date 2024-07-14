b162f7d590048fca268d5463b56f38ae2c3bfbe0
nico-slk <nico1991.6@gmail.com>
2024-07-14 02:47:17 -0300

    dates and format

--- src/main/java/it/factor/shopping_cart_backend/controller/SpecialDateController.java
+++ src/main/java/it/factor/shopping_cart_backend/controller/SpecialDateController.java
@@ -1,9 +1,36 @@
 package it.factor.shopping_cart_backend.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import it.factor.shopping_cart_backend.model.SpecialDate;
+import it.factor.shopping_cart_backend.service.SpecialDateService;
+import jakarta.websocket.server.PathParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 
 @RestController
 @RequestMapping("/date")
 public class SpecialDateController {
+
+    @Autowired
+    private SpecialDateService specialDateService;
+
+    @GetMapping("/{startDate}")
+    public ResponseEntity<SpecialDate> getSpecialDate(@PathVariable String startDate) {
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy, HH:mm:ss");
+        LocalDateTime date = LocalDateTime.parse(startDate, formatter);
+
+        return ResponseEntity.ok(specialDateService.getSpecialDateByStartDate(date));
+    }
+
+    @PostMapping("/{startDate}")
+    public ResponseEntity<SpecialDate> addSpecialDate(@RequestBody SpecialDate specialDate) {
+        return ResponseEntity.ok(specialDateService.addSpecialDate(specialDate));
+    }
+
 }
--- src/main/java/it/factor/shopping_cart_backend/service/SpecialDateService.java
+++ src/main/java/it/factor/shopping_cart_backend/service/SpecialDateService.java
@@ -1,7 +1,31 @@
 package it.factor.shopping_cart_backend.service;
 
+import it.factor.shopping_cart_backend.model.SpecialDate;
+import it.factor.shopping_cart_backend.repository.SpecialDateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Service
 public class SpecialDateService {
+    @Autowired
+    private SpecialDateRepository specialDateRepository;
+
+    public SpecialDate getSpecialDateByStartDate(LocalDateTime startDate) {
+        return specialDateRepository.findByStartDate(startDate);
+    }
+
+    public SpecialDate addSpecialDate(SpecialDate specialDate) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy, HH:mm:ss");
+
+        LocalDateTime startDate = LocalDateTime.parse(specialDate.getStartDate().toString(), formatter);
+        LocalDateTime endDate = LocalDateTime.parse(specialDate.getStartDate().toString(), formatter);
+
+        specialDate.setStartDate(startDate);
+        specialDate.setEndDate(endDate);
+
+        return specialDateRepository.save(specialDate);
+    }
 }
